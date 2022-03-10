bae7b6c70f6fc6e8edcde9e6e4f61d5f50b532c3
69codes <taiwoyusuf24@gmail.com>
2022-03-10 23:38:47 +0100

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -16,7 +16,7 @@ followed by a new line"
 */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle")
+    puts("\"Programming is like building a multilingual puzzle");
 
     return (0);
 }
