a9f7de4b3b5784c478687875f5637c1bd04c2a29
69codes <taiwoyusuf24@gmail.com>
2022-03-11 00:54:25 +0100

    g

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
-/**
- * main - Entry point
- * Return: Always 0 (Success)
+/*
+ * File: 5-printf.c
+ * Auth: Taiwo Yusuf
  */
 
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     printf("with proper grammar, but the outcome is a piece of art,\n");
