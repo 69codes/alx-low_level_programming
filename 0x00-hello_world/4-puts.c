2d60b9a65daaedd4fc16c4ccc829227e01abe86b
69codes <taiwoyusuf24@gmail.com>
2022-03-10 23:32:49 +0100

    Edited with Vscode

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+/**
+ * @file 4-puts.c
+ * @author Taiwo Yusuf
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <studio.h>
+/*
+*main - prints "Programming is like building a multilingual puzzle
+followed by a new line"
+*/
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle")
+
+    return (0);
+}
