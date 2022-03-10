cd714b78bc61b088b75c8b01821c88713dcb7331
69codes <taiwoyusuf24@gmail.com>
2022-03-10 23:43:46 +0100

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,19 @@
-/**
- * @file 4-puts.c
- * @author Taiwo Yusuf
- * @brief 
- * @version 0.1
- * @date 2022-03-10
- * 
- * @copyright Copyright (c) 2022
- * 
+/*
+ * File: 4-puts.c
+ * Auth: Taiwo Yusuf
  */
 
 #include <stdio.h>
-/*
-*main - prints "Programming is like building a multilingual puzzle
-followed by a new line"
-*/
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-    return (0);
+	return (0);
 }
