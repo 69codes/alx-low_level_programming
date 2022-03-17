d8c3f501946c542c22724f2c0b040c81a4a75c58
69codes <taiwoyusuf24@gmail.com>
2022-03-18 00:26:57 +0100

    g

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,15 @@
+/*
+ * File: 0-isupper.c
+ * Auth: Taiwo Yusuf
+ */
+
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- *  the character to be checked
- * Return: 1 if c is uppercase, 0 otherwise
+ * _isupper - Checks for uppercase characters.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is uppercase, 0 otherwise.
  */
 int _isupper(int c)
 {
